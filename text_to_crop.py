508be9620883b170409984f9763337d7937148c7
Skyler Burger <skylerburger@gmail.com>
2019-07-20 21:53:06 -0700

    recognition and cropping, though wrong direction

--- text_to_crop.py
+++ text_to_crop.py
@@ -7,11 +7,13 @@ import pytesseract
 def image_query(text_query, image_location):
     # 2. Convert image to image string
     image_string = image_to_string(image_location)
-    image_boxes = locate_text_matches(text_query, image_location, image_string)
+    image_boxes = locate_text_match(text_query, image_location, image_string)
     if image_boxes:
         # TODO: Determine the xy coords of the box containing the query string.
-        
+        crop_box = determine_crop_box(text_query, image_boxes)
+        crop_image(crop_box, image_location)
     else:
+        print('Sorry not found.')
         return
 
 def image_to_string(input_image):
@@ -26,13 +28,13 @@ def image_to_string(input_image):
             image_string = pytesseract.image_to_string(Image.open(f'input_images/{input_image}'))
             found = True
         except FileNotFoundError:
-            print(f'\nThe file {input_image} was not found.')
+            print(f'\nThe file [{input_image}] was not found.')
             input_image = input('Which image would you like to search?: ')
 
-    print(f'\nI recognized:\n {image_string}')
+    print(f'\nRecognized Text:\n {image_string}')
     return image_string
 
-def locate_text_matches(text_query, image_location, image_string):
+def locate_text_match(text_query, image_location, image_string):
     """
     Takes in a string representing the text query.
     Takes in a string representing the image's location
@@ -45,10 +47,54 @@ def locate_text_matches(text_query, image_location, image_string):
 
     if match_object:
         # a. If match found, covert image to boxes
-        print('\nI found: ', match_object.group(2))
+        print('\nMatched: ', match_object.group(2))
         return pytesseract.image_to_boxes(Image.open(f'input_images/{image_location}'))
     else:
         # b. If no match found, return message
-        print('Text query was not located in the input image.')
+        print('Search query was not located in the image.')
         return None
 
+def determine_crop_box(text_query, text_boxes):
+    text_boxes = text_boxes.split('\n')
+    char_boxes = []
+    counter = 0
+
+    # Pull out the box lines related to the text query
+    for line in text_boxes:
+        if counter > len(text_query) - 1:
+            break
+        
+        if line[0] == text_query[counter]:
+            char_boxes.append(line)
+            counter += 1
+        elif line[0] == text_query[0]:
+            char_boxes = [line]
+            counter = 1
+        else:
+            char_boxes = []
+            counter = 0
+
+    char_boxes = [char_boxes[0], char_boxes[-1]]
+    coordinates = {}
+    regex_object = re.compile(r'.\s(\d*)\s(\d*)\s(\d*)\s(\d*)')
+    match_object = regex_object.search(char_boxes[0])
+    coordinates['x_top_left'] = match_object.group(1)
+    coordinates['y_top_left'] = match_object.group(2)
+    match_object = regex_object.search(char_boxes[1])
+    coordinates['x_bottom_right'] = match_object.group(3)
+    coordinates['y_bottom_right'] = match_object.group(4)
+
+    return coordinates
+
+def crop_image(coordinates, image_location):
+    original_image = Image.open(f'input_images/{image_location}')
+    
+    xtl = int(coordinates['x_top_left'])
+    ytl = int(coordinates['y_top_left'])
+    xbr = int(coordinates['x_bottom_right'])
+    ybr = int(coordinates['y_bottom_right'])
+    
+    
+    new_image = original_image.crop((xtl, ytl, xbr, ybr))
+    new_image.save('output.jpg')
+    Image.open('output.jpg')
